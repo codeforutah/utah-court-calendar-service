54fa06aebaee580f93251b622fa95f5d848a624e
MJ Rossetti <mrossetti@codeforamerica.org>
2016-02-24 00:29:44 -0700

    its alive #2

--- app/controllers/api/v0/api_controller.rb
+++ app/controllers/api/v0/api_controller.rb
@@ -3,20 +3,26 @@ class Api::V0::ApiController < ApplicationController
   # GET api/v0/event-search.json
   # GET api/v0/event-search.json?case_number=SLC%20161901292
   def event_search
-    custom_params = params.reject{|k,v| ["controller","format","action"].include?(k) }
-
-    #eligible_params = []
+    search_params = params.reject{|k,v| ["controller","format","action"].include?(k) }
+    case_number = params["case_number"]
 
     @response = {
-      :search => {:processed_at => Time.zone.now, :params => custom_params},
-      #:errors => [{:code => 0, :message => "No results matched your search. Please try again."}],
-      :response => []
+      :request => {:received_at => Time.zone.now, :params => search_params},
     }
 
+    results = []
 
+    if case_number
+      results = CourtCalendarEvent.quarantined
+      results = results.where(:case_number => case_number)
+    end
 
+    results = results.map{|event| event.search_result} if results.any?
 
-
+    @response.merge!({
+      :processed_at => Time.zone.now,
+      :results => results,
+    })
 
     respond_to do |format|
       format.json { render json: @response }
--- app/models/court_calendar.rb
+++ app/models/court_calendar.rb
@@ -1,9 +1,13 @@
 class CourtCalendar < ActiveRecord::Base
   belongs_to :court, :inverse_of => :court_calendars
   has_many :court_calendar_pages, :inverse_of => :court_calendar
-  has_many :court_calendar_events, :inverse_of => :court_calendar
+  has_many :events, :inverse_of => :calendar, :class_name => CourtCalendarEvent
 
   serialize(:parsing_errors, Array)
+
+  delegate :name, :to => :court, :prefix => true
+  delegate :title, :to => :court, :prefix => true
+  delegate :type, :to => :court, :prefix => true
 
   def date
     requested_at.to_date
@@ -13,11 +17,7 @@ class CourtCalendar < ActiveRecord::Base
     "#{court.name} #{court.type.titlecase} -- #{url} -- #{page_count}"
   end
 
-  def pages
-    court_calendar_pages
-  end
-
-  def events
-    court_calendar_events
+  def modified_on
+    modified_at.to_date
   end
 end
