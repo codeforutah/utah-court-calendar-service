ea39c4591ef12db80a93c4e01049cb41f5769cd0
MJ Rossetti <mrossetti@codeforamerica.org>
2016-03-01 01:07:58 -0700

    #17

--- app/controllers/court_calendars_controller.rb
+++ app/controllers/court_calendars_controller.rb
@@ -1,5 +1,5 @@
 class CourtCalendarsController < ApplicationController
   def index
-    @court_calendars = CourtCalendar.most_recent
+    @court_calendars = CourtCalendar.most_recent.order(:page_count => :desc)
   end
 end
